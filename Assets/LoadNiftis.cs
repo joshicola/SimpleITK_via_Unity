6dac3393247dce5dadccde0301cea419e4a4180c
Joshua Jacobs <joshua.jacobs@mayo.edu>
2017-03-07 21:00:03 -0600

    add the directory

---
+++ Assets/LoadNiftis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using itk.simple;
+
+public class LoadNiftis : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Image input = SimpleITK.ReadImage ("/home/josher/UnityProjects/Read_Niftis/Assets/Ax_SWI.nii.gz");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
